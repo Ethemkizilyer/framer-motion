d33f80756ea33b7fba5cf5c3ad164ddf0a668286
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-02-19 02:51:29 +0300

    feat: boxs updated

--- src/pages/What.jsx
+++ src/pages/What.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 // const Container = styled.div`
@@ -26,29 +27,39 @@ const MotionBox = styled(motion.div)`
 
 const What = () => {
 
+const user = useSelector((state) => state.auth.user);
+
   return (
     <div className="mt-20 flex items-center justify-center w-[100vw] h-[90vh] border bg-red-100">
       {" "}
       <MotionBox
         drag
         dragConstraints={{ left: -460, right: 1066, top: -290, bottom: 290 }}
-        className="bg-orange-900"
-      />
+        className="bg-orange-900 flex items-center justify-center "
+      >
+        <span className="font-bold uppercase"> {user?.username}</span>
+      </MotionBox>
       <MotionBox
         drag
         dragConstraints={{ left: -650, right: 875, top: -290, bottom: 290 }}
-        className="bg-yellow-500"
-      />
+        className="bg-yellow-500 flex items-center justify-center "
+      >
+        <span className="font-bold uppercase"> {user?.username}</span>
+      </MotionBox>
       <MotionBox
         drag
         dragConstraints={{ left: -850, right: 680, top: -290, bottom: 290 }}
-        className="bg-blue-900"
-      />
+        className="bg-blue-900 flex items-center justify-center "
+      >
+        <span className="font-bold uppercase"> {user?.username}</span>
+      </MotionBox>
       <MotionBox
         drag
         dragConstraints={{ left: -1050, right: 480, top: -290, bottom: 290 }}
-        className="bg-light-green-900"
-      />
+        className="bg-light-green-900 flex items-center justify-center "
+      >
+        <span className="font-bold uppercase"> {user?.username}</span>
+      </MotionBox>
     </div>
   );
 }
