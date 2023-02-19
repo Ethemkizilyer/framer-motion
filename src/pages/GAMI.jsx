9b171d0b4ea6c60a7fce414aa22bac9c54020348
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-02-19 22:51:01 +0300

    feat: gami page added

--- src/pages/GAMI.jsx
+++ src/pages/GAMI.jsx
@@ -1,7 +1,108 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { useState } from "react";
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background: green;
+  position: relative;
+  display: flex;
+  padding: 1rem;
+  justify-content: flex-end;
+  z-index: 2;
+  padding-right: 2rem;
+`;
+
+export const Nav = styled(motion.nav)`
+  background-color: red;
+  height: 90vh;
+  width: 100vw;
+  position: absolute;
+  top: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Link = styled(motion.li)`
+  color: white;
+  margin-bottom: 1.6rem;
+  font-size: 1.4rem;
+`;
+
+export const SvgBox = styled(motion.div)``;
 
 const GAMI = () => {
-  return <div className="mt-20 w-100 h-[90vh] border bg-orange-100">GAMI</div>;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const iconVariants = {
+    opened: {
+      rotate: 135,
+    },
+    closed: {
+      rotate: 0,
+    },
+  };
+
+  const menuVariants = {
+    opened: {
+      top: 0,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.5,
+      },
+    },
+    closed: {
+      top: "-90vh",
+    },
+  };
+
+  const linkVariants = {
+    opened: {
+      opacity: 1,
+      y: 50,
+    },
+    closed: {
+      opacity: 0,
+      y: 0,
+    },
+  };
+  return (
+    <div className="mt-20 w-100 h-[90vh] border bg-orange-100 relative overflow-hidden">
+      <Header>
+        <SvgBox
+          variants={iconVariants}
+          animate={isOpen ? "opened" : "closed"}
+          whileHover={{ scale: 1.4 }}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 4C11.4477 4 11 4.44772 11 5V11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H11V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19V13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H13V5C13 4.44772 12.5523 4 12 4Z"
+              fill="#fff"
+            />
+          </svg>
+        </SvgBox>
+      </Header>
+
+      <Nav
+        initial={false}
+        variants={menuVariants}
+        animate={isOpen ? "opened" : "closed"}
+      >
+        <Link variants={linkVariants}>home</Link>
+        <Link variants={linkVariants}>about</Link>
+        <Link variants={linkVariants}>gallery</Link>
+      </Nav>
+    </div>
+  );
 }
 
 export default GAMI
