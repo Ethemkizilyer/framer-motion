d8aeb392885728ae473b4f834ced55b53d18b31d
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-02-21 02:41:13 +0300

    feat: slider added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,35 @@ import "../App.css";
 import { motion, useTransform, useScroll, useSpring } from "framer-motion";
 import { useSelector } from "react-redux";
 import Tilty from "react-tilty";
+import { useRef, useState } from "react";
+
 function Home() {
+
+ const [say,setSay]=useState(0)
   const user = useSelector((state) => state.auth.user);
 
+
+ const slideContainerRef = useRef(null);
+
+ const handleSlide = (direction) => {
+   const slideContainer = slideContainerRef.current;
+
+   if (direction === "left") {
+   setSay(say-1)
+     slideContainer.scrollBy({
+       left: -240,
+       behavior: "smooth",
+     });
+   } else {
+  setSay(say + 1);
+     slideContainer.scrollBy({
+       left: 240,
+       behavior: "smooth",
+     });
+   }
+ };
+
   let { scrollYProgress } = useScroll();
-  console.log(user);
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 10000,
     damping: 1000,
@@ -29,26 +53,105 @@ function Home() {
     show: { opacity: 1, x: 0 },
   };
   return (
-    <div >
+    <div>
       <motion.div
         style={{ scaleX: scaleX }}
         className="bg-gradient-to-r from-yellow-400 to-red-500 fixed top-0 left-0 right-0 h-[4px] origin-left -z-10"
       />
+      <Tilty className="w-64 h-64 bg-orange-500 rounded-lg mt-20 shadow-md overflow-hidden transform transition-transform duration-300 absolute  left-[40%] hover:-translate-y-2 z-[1]">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+          <h2 className="text-2xl font-bold mb-2">Welcome to my website</h2>
+          <p className="text-lg text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </Tilty>
+      <div className="relative left-8 p-8 w-[350px] flex items-center justify-center h-[300px]   border-red-800 bg-green-500">
+        {" "}
+        <button
+          disabled={say == 0}
+          onClick={() => handleSlide("left")}
+          className={`${
+            say == 0 && "hidden"
+          } absolute flex items-center justify-center left-2 text-[22px] bg-blue-800 p-1 rounded-[100%]`}
+        >
+          ◀
+        </button>
+        <div
+          ref={slideContainerRef}
+          className="w-[240px] absolute z-20 h-[200px] bg-blue-gray-400 border flex  items-center justify-left overflow-hidden transition-width duration-1000"
+        >
+          <Tilty>
+            <div className="min-w-[100px] flex  items-center justify-center mx-[10px] h-[100px] bg-orange-400">
+              1
+            </div>
+          </Tilty>
+          <Tilty>
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              2
+            </div>
+          </Tilty>
+          <Tilty>
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              3
+            </div>
+          </Tilty>
+          <Tilty>
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              4
+            </div>
+          </Tilty>
+          <Tilty>
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              5
+            </div>
+          </Tilty>
+          <Tilty>
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              6
+            </div>
+          </Tilty>
+          <Tilty>
+            {" "}
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              7
+            </div>
+          </Tilty>
+          <Tilty>
+            {" "}
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              8
+            </div>
+          </Tilty>
+          <Tilty>
+            {" "}
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              9
+            </div>
+          </Tilty>
+          <Tilty>
+            <div className="min-w-[100px] flex  items-center justify-center  mx-[10px] h-[100px] bg-orange-400">
+              10
+            </div>
+          </Tilty>
+        </div>
+        <button
+          disabled={say == 5}
+          onClick={() => handleSlide("right")}
+          className={`${
+            say >= 4 && "hidden"
+          } absolute flex items-center justify-center right-2 text-[22px] bg-blue-800 p-1 rounded-[100%] `}
+        >
+          ▶
+        </button>
+      </div>
+
       <motion.div
         className="w-full flex flex-col gap-20 py-20"
         variants={container}
         initial="hidden"
         whileInView="show"
       >
-        <Tilty className="w-64 h-64 bg-orange-500 rounded-lg shadow-md overflow-hidden transform transition-transform duration-300 absolute top-[25vh] left-[40%] hover:-translate-y-2 z-[1]">
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-            <h2 className="text-2xl font-bold mb-2">Welcome to my website</h2>
-            <p className="text-lg text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </div>
-        </Tilty>
-        
         <motion.div
           initial={{ opacity: 1 }}
           style={{ x: y, rotateZ: y, scale: 2 }}
@@ -68,7 +171,7 @@ function Home() {
           variants={item}
           initial={{ opacity: 1 }}
           style={{ x: y, rotateZ: y, scale: 2 }}
-          className="bg-red-500 w-[160px] h-[160px] left-[160px] top-[1050px] flex -z-50 absolute items-center justify-center"
+          className="bg-red-500 w-[160px] h-[160px] left-[80px] top-[1050px] flex -z-50 absolute items-center justify-center"
         >
           {user?.username}
         </motion.div>
