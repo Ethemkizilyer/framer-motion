28f32e9f17710ae81dde265e533776034a07c6fc
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-03-06 23:38:08 +0300

    feat: progrees bar added

--- src/pages/What.jsx
+++ src/pages/What.jsx
@@ -1,7 +1,47 @@
 import { motion } from 'framer-motion';
-import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { Box } from "@chakra-ui/core";
+import { useEffect, useState } from "react";
+
+const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      if (progress >= 75) {
+        clearInterval(timerId);
+        return;
+      }
+      setProgress(progress + 1);
+    }, 10);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [progress]);
+
+  const progressStyle = {
+    width: `${progress}%`,
+    height: "20px",
+    background: `linear-gradient(to right, 
+    #ff0040 0%,
+      #d9ff00 25%,
+      #008700 50%,
+      #090087 75%)`,
+    borderRadius: "10px",
+  };
+
+  return (
+    <Box width="100%" backgroundColor="#ddd" borderRadius="10px">
+      <motion.div
+        style={progressStyle}
+        animate={{ width: `${progress}%` }}
+        transition={{ duration: 0.1 }}
+      />
+    </Box>
+  );
+};
 
 const Container = styled.div`
   background-color: red;
@@ -31,7 +71,7 @@ const user = useSelector((state) => state.auth.user);
 
   return (
     <div className="mt-20 flex items-center justify-center w-[100vw] h-[90vh] border bg-red-100">
-      {" "}
+      <ProgressBar/>{" "}
       <MotionBox
         drag
         dragConstraints={{ left: -460, right: 1066, top: -290, bottom: 290 }}
